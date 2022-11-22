6576226829ebfdee8a1e9190489f65f4dd76ae41
DaLittleRedBird <107221162+DaLittleRedBird@users.noreply.github.com>
2022-11-22 16:56:32 -0500

    Update index.js

--- gruffalo/index.js
+++ gruffalo/index.js
@@ -1,28 +1,19 @@
 
-const { generateStates } = require('./states')
-const { parse } = require('./glr')
+const { generateStates } = require('./states'), { parse } = require('./glr');
 
 function parserFor(grammar) {
-  let states = generateStates(grammar)
-  let start = grammar.start
+    let states = generateStates(grammar), start = grammar.start;
 
-  // console.log(states.map(state => state.debug()).join('\n'))
+    // console.log(states.map(state => state.debug()).join('\n'));
 
-  // const fs = require('fs')
-  // fs.writeFileSync(grammar.start + '.bnf', grammar.debug(), 'utf-8')
+    // const fs = require('fs');
+    // fs.writeFileSync(grammar.start + '.bnf', grammar.debug(), 'utf-8');
 
-  // let content = 'digraph G {\nrankdir=LR\n' + states.map(state => state.toDot()).join('\n') + '\n}'
-  // fs.writeFileSync(grammar.start + '.dot', content, 'utf-8')
+    // let content = 'digraph G {\nrankdir=LR\n' + states.map(state => state.toDot()).join('\n') + '\n}';
+    // fs.writeFileSync(grammar.start + '.dot', content, 'utf-8');
 
-  return function(lex) {
-    return parse(states[0], start, lex)
-  }
+    return function(lex) { return parse(states[0], start, lex); };
 }
 
-module.exports = {
-  Grammar: require('./grammar').Grammar,
-  Rule: require('./grammar').Rule,
-  compile: require('./lr1').compile,
-  parserFor,
-}
+module.exports = { Grammar: require('./grammar').Grammar, Rule: require('./grammar').Rule, compile: require('./lr1').compile, parserFor, }
 
