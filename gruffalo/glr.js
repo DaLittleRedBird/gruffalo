0873124fc4c63d81d2fba415ec37f5fe077e75b8
DaLittleRedBird <107221162+DaLittleRedBird@users.noreply.github.com>
2022-11-22 14:38:41 -0500

    Update glr.js

--- gruffalo/glr.js
+++ gruffalo/glr.js
@@ -1,159 +1,107 @@
 
 const { LR1 } = require('./grammar');
-var highestId = 0;
+var i, j, highestGSSId = 0;
 
-function helloQuestionmark(label) {
-    //Is this going to be on the main page?
+//WARNING : The GSSNode, Path, Edge and ReduceSet objects been NOT tested yet, if you want to use Gruffalo, fork the main page.
+function GSSNode(label) {
     this.label = label;
     this.edgesById = {};
-    // data?
-    this.id = ++highestId;
+    this.id = ++highestGSSId;
 }
 
-class Node {
-  constructor(label) {
-    this.label = label
-    this.edgesById = {}
-    // data?
+GSSNode.prototype.getName = function() { return 'state #' + this.id; }
 
-    this.id = ++Node.highestId
-  }
+GSSNode.prototype.addEdge = function(node) {
+    let edge = this.edgesById[node.id];
+    if (!edge) { return this.edgesById[node.id] = new Edge(node); }
+    return edge;
+}
 
-  get name() {
-    return 'n' + this.id
-    return '?btwvuxyzacdefghijklmnopqrs'[this.id]
-  }
+// TODO: test nb. length can be out-of-bounds (return []) or zero (return [this])
+GSSNode.prototype.traverse = function(length, firstEdge) {
+    // assert(firstEdge.head.node === this);
+    let paths = [firstEdge];
 
-  addEdge(node) {
-    var edge = this.edgesById[node.id]
-    if (!edge) {
-      return this.edgesById[node.id] = new Edge(node)
-    }
-    return edge
-  }
-
-  // TODO: test
-  // nb. length can be out-of-bounds (return [])
-  // or zero (return [this])
-  traverse(length, firstEdge) {
-    // assert(firstEdge.head.node === this)
-    let paths = [firstEdge]
-
-    for ( ; length--; ) {
-      let newPaths = []
-      for (var i = paths.length; i--; ) {
-        let path = paths[i]
-        let begin = path ? path.head.node : this
-        let edges = begin.edgesById
-        let keys = Object.keys(edges)
-        for (var j = keys.length; j--; ) {
-          let edge = edges[keys[j]]
-          newPaths.push(new Path(edge, path))
+    for (; length--;) {
+      let newPaths = [];
+      for (i = paths.length; i--; ) {
+        
+        let path = paths[i], begin = path ? path.head.node : this;
+        let edges = begin.edgesById, keys = Object.keys(edges);
+        
+        for (j = keys.length; j--; ) {
+          let edge = edges[keys[j]];
+          newPaths.push(new Path(edge, path));
         }
+        
       }
-      paths = newPaths
+      paths = newPaths;
     }
-    return paths
-  }
-}
-Node.highestId = 0
-
-
-class Path {
-  constructor(head, tail) {
-    this.head = head
-    this.tail = tail
-  }
-
-  toArray() {
-    let out = []
-    let l = this
-    do {
-      out.push(l.head.data)
-    } while (l = l.tail)
-    return out
-  }
+    return paths;
 }
 
-
-class Edge {
-  constructor(node) {
-    this.node = node
-
-    this.data = null
-  }
-
-  /*
-   * The RNGLR paper uses Rekers for better memory efficiency.
-   * Use Tomita's SPPF algorithm since it's simpler & faster
-   */
-  addDerivation(rule, path) {
-    let children = path ? path.toArray() : []
-
-    // console.log('' + rule, children)
-    let data = rule.build.apply(null, children)
-
-    if (this.data !== null) {
-      throw 'wow'
-    }
-    this.data = data
-  }
-
-  debug() {
-    return this.data
-  }
+function Path(head, tail) { //A valid GLR stack.
+    this.head = head; //Top of the stack
+    this.tail = tail; //Rest of the stack
 }
 
-
-// TODO how to implement this efficiently ?
-class RSet {
-  constructor() {
-    this.reductions = []
-    this.unique = {}
-  }
-
-  add(start, item, firstEdge) {
-    let hash = start.id + '$' + item.id
-    if (!this.unique[hash]) {
-      this.reductions.push(this.unique[hash] = {start, item, firstEdge})
-    }
-  }
-
-  pop() {
-    let r = this.reductions.pop()
-    if (!r) return
-    let hash = r.start.id + '$' + r.item.id
-    delete this.unique[hash]
-    return r
-  }
+//Returns the stack corresponding to the current path.
+Path.prototype.toArray = function() {
+    let outStack = [], curEdge = this;
+    do { outStack.push(curEdge.head.data); } while (curEdge = curEdge.tail);
+    return outStack;
 }
 
+function Edge(node) { this.node = node; this.data = null; }
 
-var TOK
-var GOTO
-var DATA
-var LENGTH
+Edge.prototype.debug = function() { return this.data; }
 
-var REDUCTIONS = new RSet()
-var NODES = {}
-var TOKEN
+/* The RNGLR paper uses Rekers for better memory efficiency.
+ * Use Tomita's SPPF algorithm since it's simpler & faster.
+ */
+Edge.prototype.addDerivation = function(rule, path) {
+    let children = path ? path.toArray() : [];
+
+    // console.log('' + rule, children);
+    let data = rule.build.apply(null, children);
+
+    if (this.data !== null) { throw 'wow'; }
+    this.data = data;
+}
+
+// TODO: how to implement this efficiently, Red-Black trees?
+function ReduceSet() { this.reductions = []; this.unique = {}; }
+
+ReduceSet.prototype.insert = function(start, item, firstEdge) {
+    //Hash tables are good, maybe we can use 32-bit bitstrings?
+    let hash = start.id + '$' + item.id;
+    if (!this.unique[hash]) { this.reductions.push(this.unique[hash] = {start, item, firstEdge}); }
+}
+
+ReduceSet.prototype.pop = function() {
+    let r = this.reductions.pop();
+    if (!r) {return;}
+    let hash = r.start.id + '$' + r.item.id;
+    delete this.unique[hash];
+    return r;
+}
+
+var REDUCTIONS = new ReduceSet(), NODES = {}, TOKEN, LOOKAHEAD, GOTO, DATA, LENGTH;
 
 function push(advance, start) {
-  var node = NODES[advance.index]
+  let node = NODES[advance.index]
 
   if (!node) {
     // new node
-    var node = NODES[advance.index] = new Node(advance) // node: Node = w
+    let node = NODES[advance.index] = new GSSNode(advance); // node: Node = w
 
     /*
      * record all reductions (of length 0)
      * together with the second node along the path
      * which is `node`, since the `start` of a Reduction is the second node... ?!
      */
-    for (let item of advance.reductions[TOK] || []) { // lookup l
-      if (item.dot === 0) {
-        REDUCTIONS.add(node, item, null) // (w, B, 0)
-      }
+    for (let item of advance.reductions[LOOKAHEAD] || []) { // lookup l
+      if (item.dot === 0) { REDUCTIONS.insert(node, item, null) } // (w, B, 0)
     }
   }
 
@@ -165,66 +113,56 @@ function push(advance, start) {
    * to check those against the new path
    */
   if (LENGTH > 0) {
-    for (let item of advance.reductions[TOK] || []) {
-      if (item.dot !== 0) {
-        REDUCTIONS.add(start, item, new Path(edge)) // (v, B, t)
-      }
+    for (let item of advance.reductions[LOOKAHEAD] || []) {
+      if (item.dot !== 0) { REDUCTIONS.add(start, item, new Path(edge)); } // (v, B, t)
     }
   }
 
-  return edge
+  return edge;
 }
 
 function goSwitch(start) {
-  let advance = start.label.transitions[GOTO]
-  if (advance) {
-    return push(advance, start)
-  }
+  let advance = start.label.transitions[GOTO];
+  if (advance) { return push(advance, start); }
 }
 
 function shift(nextColumn) {
-  var OLD = NODES
-  NODES = {}
-  let keys = Object.keys(OLD)
+  let OLD = NODES, keys = Object.keys(OLD);
+  NODES = {};
 
   for (let index of keys) {
-    let start = OLD[index] // start: Node = v, advance: State = k
-    let edge = goSwitch(start)
-    if (edge) {
-      edge.data = DATA
-    }
+    let start = OLD[index]; // start: Node = v, advance: State = k
+    let edge = goSwitch(start);
+    if (edge) { edge.data = DATA; }
   }
   return nextColumn
 }
 
 function reduce(item, start, firstEdge) {
-  let length = item.dot
-  let rule = item.rule
-  let target = rule.target // target = X
-  // console.log('(', start.name, target, length, ')')
-  let set = start.traverse(Math.max(0, length - 1), firstEdge)
-
-  let edge
+  let length = item.dot, rule = item.rule, target = rule.target; // target = X
+    
+  // console.log('(', start.name, target, length, ')');
+  let set = start.traverse(Math.max(0, length - 1), firstEdge), edge;
+    
   for (let path of set) {
     let begin = path ? path.head.node : start
 
-    // if (path) assert(begin === path.head.node)
+    // if (path) {assert(begin === path.head.node);}
 
     // begin.label: State = k
 
-    LENGTH = length
-    GOTO = target
-    let edge = goSwitch(begin)
-    // assert(edge)
-    edge.addDerivation(rule, path, firstEdge)
+    LENGTH = length; GOTO = target;
+    let edge = goSwitch(begin);
+    // assert(edge);
+    edge.addDerivation(rule, path, firstEdge);
   }
 }
 
 function reduceAll() {
-  var reduction
+  var reduction;
   while (reduction = REDUCTIONS.pop()) {
-    let { start, item, firstEdge } = reduction // length: Int = m
-    reduce(item, start, firstEdge)
+    let { start, item, firstEdge } = reduction; // length: Int = m
+    reduce(item, start, firstEdge);
   }
 }
 
@@ -232,54 +170,45 @@ function reduceAll() {
 
 function parse(startState, target, lex) {
   // TODO don't hardcode grammar start symbol
-  let acceptingState = startState.transitions[target]
+  let acceptingState = startState.transitions[target];
 
   // TODO handle empty input
 
-  TOKEN = lex()
+  TOKEN = lex();
+  NODES = {};
+  let startNode = NODES[startState.index] = new Node(startState);
+  for (let item of startState.reductions[TOKEN.type] || []) { REDUCTIONS.insert(startNode, item, null); } // TODO test this
 
-  NODES = {}
-  let startNode = NODES[startState.index] = new Node(startState)
-  for (let item of startState.reductions[TOKEN.type] || []) {
-    REDUCTIONS.add(startNode, item, null) // TODO test this
-  }
-
-  var count = 0
+  let count = 0;
   do {
-    reduceAll()
-    // console.log(column.debug())
-    // console.log(column.reductions)
+    reduceAll();
+    // console.log(column.debug());
+    // console.log(column.reductions);
 
-    GOTO = TOKEN.type
-    DATA = TOKEN
+    GOTO = TOKEN.type;
+    DATA = TOKEN;
 
-    TOKEN = lex()
-    TOK = TOKEN.type
-    LENGTH = 1
+    TOKEN = lex();
+    LOOKAHEAD = TOKEN.type;
+    LENGTH = 1;
 
-    shift(NODES)
+    shift(NODES);
 
     // check column is non-empty
-    if (Object.keys(NODES).length === 0) {
-      throw new Error('Syntax error @ ' + count + ': ' + JSON.stringify(TOKEN.type))
-    }
-    count++
-  } while (TOKEN.type !== LR1.EOF)
+    if (Object.keys(NODES).length === 0) { throw new Error('Syntax error @ ' + count + ': ' + JSON.stringify(TOKEN.type)); }
+    count++;
+  } while (TOKEN.type !== LR1.EOF);
 
-  reduceAll()
-  // console.log(column.debug())
+  reduceAll();
+  // console.log(column.debug());
 
-  let finalNode = NODES[acceptingState.index]
-  if (!finalNode) {
-      throw new Error('Unexpected end of input')
-  }
+  let finalNode = NODES[acceptingState.index];
+  if (!finalNode) { throw new Error('Unexpected end of input'); }
 
-  let rootEdge = finalNode.edgesById[startNode.id]
+  let rootEdge = finalNode.edgesById[startNode.id];
 
-  return rootEdge.data
+  return rootEdge.data;
 }
 
-module.exports = {
-  parse,
-}
+module.exports = { parse, }
 
