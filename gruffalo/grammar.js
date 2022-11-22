538de922612e195fb6e0904c51a8f06f656a65b5
DaLittleRedBird <107221162+DaLittleRedBird@users.noreply.github.com>
2022-11-22 15:15:15 -0500

    Update grammar.js

--- gruffalo/grammar.js
+++ gruffalo/grammar.js
@@ -1,243 +1,149 @@
+var i, highestGrammarID = 0, highestCLRStateID = 0;
+const CLREOF = "$";
 
-class Rule {
-  constructor(target, symbols, build) {
-    if (!symbols || symbols.constructor !== Array) {
-      throw 'symbols must be a list'
-    }
+//WARNING : The ContextFreeGrammar, GrammarRule and CLR functions have NOT been tested, if you want to use Gruffalo, fork the original code.
+function GrammarRule(target, symbols, build) {
+    if (!symbols || symbols.constructor !== Array) { throw 'symbols must be a list'; }
     if (typeof build !== 'function') {
       // TODO opt
-      build = eval('(function (...args) { return [' + JSON.stringify(target) + ', args] })')
+      build = eval('(function (...args) { return [' + JSON.stringify(target) + ', args] })');
     }
-    this.symbols = symbols
-      if (symbols.length > 0 && symbols[0] === undefined) { throw new Error() }
-    this.target = target
-    this.build = build
-    this._items = {}
-    this.id = ++Rule.highestId
-  }
-
-  startItem(lookahead) {
-    if (this._items[lookahead]) { return this._items[lookahead] }
-    let symbols = this.symbols
-    if (!symbols.length) {
-      return this._items[lookahead] = new LR1(this, 0, lookahead)
-    }
-    var previous
-    var first = previous = new LR1(this, 0, lookahead)
-    for (var dot=1; dot<symbols.length; dot++) {
-      let lr0 = new LR1(this, dot, lookahead)
-      previous.advance = lr0
-      previous = lr0
-    }
-    previous.advance = new LR1(this, dot, lookahead)
-    return this._items[lookahead] = first
-  }
-
-  toString() {
-    return this.target.toString() + ' → ' + this.symbols.join(' ')
-  }
-
-  reverse() {
-    let clone = new Rule(this.target, reversed(this.symbols), null)
-    clone.priority = this.priority
-    clone._original = this
-    return clone
-  }
+    this.symbols = symbols;
+    if (symbols.length > 0 && symbols[0] === undefined) { throw new Error() }
+    this.target = target;
+    this.build = build;
+    this._items = {};
+    this.id = ++highestGrammarID;
 }
-Rule.highestId = 0
 
-class LR1 {
-  constructor(rule, dot, lookahead) {
-    this.id = ++LR1.highestId
-    this.rule = rule
-    this.wants = rule.symbols[dot]
-    this.dot = dot
-    this.advance = null // set by Rule
-    this.lookahead = lookahead
+GrammarRule.prototype.startItem = function(lookahead) {
+    if (this._items[lookahead]) { return this._items[lookahead]; }
+    let symbols = this.symbols;
+    if (!symbols.length) { return this._items[lookahead] = new LR1(this, 0, lookahead); }
+    let previous, first = previous = new CLR(this, 0, lookahead);
+    for (let dot = 1; dot < symbols.length; dot++) {
+      let lr0 = new CLR(this, dot, lookahead);
+      previous.advance = lr0;
+      previous = lr0;
+    }
+    previous.advance = new CLR(this, dot, lookahead);
+    return this._items[lookahead] = first;
+}
+
+GrammarRule.prototype.toString = function() { return this.target.toString() + ' → ' + this.symbols.join(' '); }
+
+GrammarRule.prototype.reverse = function() {
+    let clone = new GrammarRule(this.target, reversed(this.symbols), null);
+    clone.priority = this.priority;
+    clone._original = this;
+    return clone;
+}
+
+//Slow, but very reliable.
+function CLR(rule, dot, lookahead) {
+    this.id = ++highestCLRStateID;
+    this._wlh = null;
+    this.rule = rule;
+    this.wants = rule.symbols[dot];
+    this.dot = dot;
+    this.advance = null; // set by rule
+    this.lookahead = lookahead;
+    this.getHash = function() { return this.rule.id + '$' + this.dot; }
+    this.isAccepting = function() { return this.rule.isAccepting && this.wants === undefined && this.lookahead == CLREOF; }
+    
     if (typeof lookahead !== 'string') { throw new Error(JSON.stringify(lookahead)) }
-  }
-
-  get hash() {
-    return this.rule.id + '$' + this.dot
-  }
-
-  get isAccepting() {
-    return this.rule.isAccepting && this.wants === undefined && this.lookahead == LR1.EOF
-  }
-
-  // TODO: test
-  isRightNullable(grammar) {
-    let symbols = this.rule.symbols
-    for (var i = this.dot; i < symbols.length; i++) {
-      if (!grammar.firstTerminalFor(symbols[i])['$null']) {
-        return false
-      }
-    }
-    return true
-  }
-
-  toString() {
-    let symbols = this.rule.symbols.slice()
-    symbols.splice(this.dot, 0, '•')
-    let lookahead = this.lookahead === LR1.EOF ? '$' : this.lookahead
-    return this.rule.target.toString() + ' → ' + symbols.map(x => x.toString()).join(' ') + ' :: ' + lookahead
-  }
-
-  wantsLookahead(grammar) {
-    if (this._wlh) return this._wlh
-    let after = this.rule.symbols.slice(this.dot + 1)
-    let out = {}
-    for (var terminal in this.lookahead) {
-      let terminals = grammar.firstTerminal(after.concat(terminal))
-      for (var key in terminals) {
-        out[key] = true
-      }
-    }
-    return this._wlh = out
-  }
 }
-LR1.highestId = 0
-LR1.EOF = '$'
 
+//TODO : test this rigourously
+CLR.prototype.isRightNullable = function(grammar) {
+    //This is the function that makes this GLR parser possible in the first place.
+    let symbols = this.rule.symbols;
+    for (i = this.dot; i < symbols.length; i++) { if (!grammar.firstTerminalFor(symbols[i])['$null']) { return false; } }
+    return true;
+}
 
-class Grammar {
-  constructor(options) {
-    this.rules = []
-    this.ruleSets = {} // rules by target
-    this.start = options.start
-    this.highestPriority = 0
+CLR.prototype.toString = function() {
+    let symbols = this.rule.symbols.slice();
+    symbols.splice(this.dot, 0, '•');
+    let lookahead = this.lookahead === CLREOF ? '$' : this.lookahead;
+    return this.rule.target.toString() + ' → ' + symbols.map(x => x.toString()).join(' ') + ' :: ' + lookahead;
+}
 
-    this._listFirst = {}
-    this._symbolFirst = {}
-  }
+CLR.prototype.wantsLookahead = function(grammar) {
+    if (this._wlh) { return this._wlh; }
+    let after = this.rule.symbols.slice(this.dot + 1), out = {};
+    for (let terminal in this.lookahead) {
+        let terminals = grammar.firstTerminal(after.concat(terminal))
+        for (let key in terminals) { out[key] = true; }
+    }
+    this._wlh = out;
+    return this._wlh;
+}
 
-  static fromNearley(compiled) {
-    function name(name) {
-      return '$' + name
+function ContextFreeGrammar(options) {
+    this.rules = [];
+    this.ruleSets = {}; // rules by target
+    this.start = options.start;
+    this.highestPriority = 0;
+    this._listFirst = {};
+    this._symbolFirst = {};
+    this.isTerminal = function(symbol) { return !this.ruleSets[symbol]; }
+}
+
+ContextFreeGrammar.prototype.addProductionRule = function(rule) {
+    this.rules.push(rule);
+    if (!(rule instanceof GrammarRule)) {throw 'not a rule';}
+    rule.priority = ++this.highestPriority;
+    var set = this.ruleSets[rule.target];
+    if (!set) { this.ruleSets[rule.target] = set = []; }
+    set.push(rule);
+}
+
+ContextFreeGrammar.prototype.getProductionRule = function(target) { return this.ruleSets[target]; }
+
+ContextFreeGrammar.prototype.debug = function() {
+    let rules = [];
+    Object.keys(this.ruleSets).forEach(target => { let ruleSet = this.ruleSets[target]; ruleSet.forEach(rule => { rules.push(rule.toString()); }); });
+    return rules.join('\n');
+}
+
+//TODO : test this rigourously
+ContextFreeGrammar.prototype.firstTerminalFor = function(symbol, seenRules) {
+    if (this._symbolFirst[symbol]) { return this._symbolFirst[symbol]; }
+
+    let rules = this.ruleSets[symbol];
+    if (!rules) { return { [symbol]: true }; } // terminal
+
+    seenRules = seenRules || {};
+
+    let result = {}, hasNull = false;
+    for (i = 0; i < rules.length; i++) {
+        let rule = rules[i];
+        if (seenRules[rule.id]) { continue; }
+        seenRules[rule.id] = true;
+
+        let symbols = rule.symbols, terminals = this.firstTerminal(symbols, seenRules);
+        for (let key in terminals) { result[key] = true; }
+
+        delete seenRules[rule.id];
     }
 
-    function regExp(sym) {
-      // TODO regex classes
-      return '/'
-    }
+    return this._symbolFirst[symbol] = result;
+}
 
-    let g = new Grammar({ start: name(compiled.ParserStart) })
-    compiled.ParserRules.forEach(r => {
-      var build
-      if (r.postprocess) {
-        let args = []
-        let names = []
-        for (var i = 0; i < r.symbols.length; i++) {
-          let arg = 'd' + i
-          args.push(arg)
-          names.push(r.symbols[i].literal ? arg + '.value' : arg)
-        }
-
-        var postprocess = r.postprocess
-        var source = ''
-        source += '(function(' + args.join(', ') + ') {\n'
-        source += 'return postprocess([' + names.join(', ') + '])\n'
-        source += '})\n'
-        build = eval(source)
-      }
-
-      let symbols = r.symbols.map(sym => sym.test ? regExp(sym) : sym.literal ? sym.literal : name(sym))
-      g.add(new Rule(name(r.name), symbols, build))
-    })
-    return g
-  }
-
-  add(rule) {
-    this.rules.push(rule)
-    if (!(rule instanceof Rule)) throw 'not a rule'
-    rule.priority = ++this.highestPriority
-    var set = this.ruleSets[rule.target]
-    if (!set) { this.ruleSets[rule.target] = set = [] }
-    set.push(rule)
-  }
-
-  get(target) {
-    return this.ruleSets[target]
-  }
-
-  debug() {
-    let rules = []
-    Object.keys(this.ruleSets).forEach(target => {
-      let ruleSet = this.ruleSets[target]
-      ruleSet.forEach(rule => {
-        rules.push(rule.toString())
-      })
-    })
-    return rules.join('\n')
-  }
-
-  isTerminal(sym) {
-    return !this.ruleSets[sym]
-  }
-
-  // TODO: test
-  firstTerminalFor(symbol, seenRules) {
-    if (this._symbolFirst[symbol]) {
-      return this._symbolFirst[symbol]
-    }
-
-    let rules = this.ruleSets[symbol]
-    if (!rules) { // terminal
-      return { [symbol]: true }
-    }
-
-    seenRules = seenRules || {}
-
-    let result = {}
-    var hasNull = false
-    for (var i = 0; i < rules.length; i++) {
-      let rule = rules[i]
-      if (seenRules[rule.id]) {
-        continue
-      }
-      seenRules[rule.id] = true
-
-      let symbols = rule.symbols
-      let terminals = this.firstTerminal(symbols, seenRules)
-      for (var key in terminals) {
-        result[key] = true
-      }
-
-      delete seenRules[rule.id]
-    }
-
-    return this._symbolFirst[symbol] = result
-  }
-
-  // TODO: test
-  firstTerminal(symbols, seenRules) {
-    if (symbols.length === 0) {
-      return { '$null': true }
-    }
-
-    let hash = symbols.join(', ')
-    if (this._listFirst[hash]) {
-      return this._listFirst[hash]
-    }
-
-    let result = {}
+//TODO : test this rigourously
+ContextFreeGrammar.prototype.firstTerminal = function(symbol, seenRules) {
+    if (symbols.length === 0) { return { '$null': true }; }
+    let hash = symbols.join(', ');
+    if (this._listFirst[hash]) { return this._listFirst[hash]; }
+    let result = {};
     for (var i = 0; i < symbols.length; i++) {
-      let terminals = this.firstTerminalFor(symbols[i], seenRules)
-      for (var key in terminals) {
-        result[key] = true
-      }
-      if (!terminals['$null']) {
-        break
-      }
+        let terminals = this.firstTerminalFor(symbols[i], seenRules)
+        for (var key in terminals) { result[key] = true; }
+        if (!terminals['$null']) { break; }
     }
-    return this._listFirst[hash] = result
-  }
+    return this._listFirst[hash] = result;
 }
 
-
-module.exports = {
-  Rule,
-  LR1,
-  Grammar,
-}
+module.exports = { GrammarRule, CLR, ContextFreeGrammar, };
 
